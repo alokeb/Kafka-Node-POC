e77e951783e3753afaee74303e58f1ff1b47e95b
alokeb <alokeb@gmail.com>
2022-05-23 16:39:45 -0700

    Iterating

--- Consumer/consumer.js
+++ Consumer/consumer.js
@@ -1,35 +1,19 @@
-const kafka = require('kafka-node');
-const express = require('express');
-const port = 3000;
-const app = express();
+const kafkaTopic = 'kafka-producer-consumer'
 
-const Consumer = kafka.Consumer,
- client = new kafka.KafkaClient('localhost:9092'),
- consumer = new Consumer(
- client, [ { topic: 'kafka-producer-consumer', partition: 0 } ], { autoCommit: false });
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.KafkaClient(),
+    consumer = new Consumer(
+        client,
+        [
+            { topic: kafkaTopic, partition: 0 } 
+        ],
+        {
+            autoCommit: false
+        }
+    );
 
-const consumerGroup = new ConsumerGroupStream(consumerOptions, 'ExampleTopic');
- 
-const messageTransform = new Transform({
-  objectMode: true,
-  decodeStrings: true,
-  transform (message, encoding, callback) {
-    console.log(`Received message ${message.value} transforming input`);
-    callback(null, {
-      topic: 'RebalanceTopic',
-      messages: `You have been (${message.value}) made an example of`
-    });
-  }
-});
-
-io.on('connection', client => {
-  console.log('Connected', client);
-
-consumer.on('message', function (message) {
-    client.emit('request', message.value);
+    consumer.on('message', function (message) {
+      console.log(message);
   });
-
-client.on('disconnect', () => { 
-    console.log('Client disconnected');
- });
-});
+  
--- Producer/producer.js
+++ Producer/producer.js
@@ -2,44 +2,24 @@
 
 const readline = require('readline')
 const fs = require('fs')
+const Transform = require('stream').Transform;
+const ProducerStream = require('./lib/producerStream')
+const _ = require('lodash')
 const kafkaTopic = 'kafka-producer-consumer'
+const producer = new ProducerStream()
 
-var kafka = require('kafka-node'),
-    Producer = kafka.Producer,
-    client = new kafka.KafkaClient(),
-    producer = new Producer(client);
+var stdinTransform = new Transform({
+  objectMode: true,
+  decodeStrings: true,
+  transform (text, encoding, callback) {
+    text = _.trim(text);
+    console.log(`pushing message ${text} to ExampleTopic`);
+    callback(null, {
+      topic: kafkaTopic,
+      messages: text
+    });
+  }
+});
 
-   /*
-   {
-
-   Here's the kafka message example
-
-   topic: 'topicName',
-   messages: ['message body'], // multi messages should be a array, single message can be just a string or a KeyedMessage instance
-   key: 'theKey', // string or buffer, only needed when using keyed partitioner
-   partition: 0, // default 0
-   attributes: 2, // default: 0
-   timestamp: Date.now() // <-- defaults to Date.now() (only available with kafka v0.10+)
-}
-*/
-
-
-//Get messages to send to Kafka (using bundled sql dump file for POC)
-let allFileContents = fs.readFileSync('broadband-plans.sql', 'utf-8')
-
-client = new kafka.KafkaClient(),
-producer = new Producer(client);
- 
-allFileContents.split(/\r?\n/).forEach(line => {
-     let payload = [
-          {
-            topic: kafkaTopic,
-            messages: line,
-          }
-        ];
-        producer.on('ready', function () {
-          producer.send(payload);
-          const used = process.memoryUsage().heapUsed / 1024 / 1024;
-          console.log('memory used: $used')
-        })
-      })
+process.stdin.setEncoding('utf8');
+process.stdin.pipe(stdinTransform).pipe(producer);
